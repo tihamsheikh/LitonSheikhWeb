59e733176ccc8d6369ba4f79398ac7907aaf423c
Tiham69 <105205393+Tiham69@users.noreply.github.com>
2024-09-02 23:21:00 +0600

    first commit

---
+++ data/index.ts
@@ -0,0 +1,226 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  // { name: "Projects", link: "#projects" },
+  { name: "Coloboration", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I value freedom and free-speach",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I like to travel around the world",
+    description: "I have already visited more than 23 countries",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My most recent works",
+    description: "",//"My works include movies that I stared in and some songs that I wrote",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  // {
+  //   id: 4,
+  //   title: "I participaited in many ",
+  //   description: "",
+  //   className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+  //   imgClassName: "",
+  //   titleClassName: "justify-start",
+  //   img: "/grid.svg",
+  //   spareImg: "/b4.svg",
+  // },
+
+  {
+    id: 5,
+    title: "Photo of my recent photoshoot",
+    description: "This photo is taken for a model agency",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/liton_1.png", // change this photo
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Contact me on WhatsApp",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Liton Sheikh’s lyrics have a magical quality—they touch the soul and evoke emotions effortlessly. His ability to craft beautiful melodies and thought-provoking lyrics is truly commendable. Liton’s songs resonate with authenticity, they feel like personal stories shared with the world. Liton’s words have the power to heal hearts and ignite passion. Thank you, Liton, for sharing your gift with us. Your music brightens our days. In a world filled with noise, Liton Sheikh’s songs stand out as genuine works of art.",
+    name: "Ross Wilson",
+    title: "Director of Rock Groups",
+  },
+  {
+    quote:
+      "Liton Sheikh’s lyrics have a magical quality—they touch the soul and evoke emotions effortlessly. His ability to craft beautiful melodies and thought-provoking lyrics is truly commendable. Liton’s songs resonate with authenticity, they feel like personal stories shared with the world. Liton’s words have the power to heal hearts and ignite passion. Thank you, Liton, for sharing your gift with us. Your music brightens our days. In a world filled with noise, Liton Sheikh’s songs stand out as genuine works of art.",
+    name: "Ross Wilson",
+    title: "Director of Rock Groups",
+  },
+  {
+    quote:
+      "Liton Sheikh’s lyrics have a magical quality—they touch the soul and evoke emotions effortlessly. His ability to craft beautiful melodies and thought-provoking lyrics is truly commendable. Liton’s songs resonate with authenticity, they feel like personal stories shared with the world. Liton’s words have the power to heal hearts and ignite passion. Thank you, Liton, for sharing your gift with us. Your music brightens our days. In a world filled with noise, Liton Sheikh’s songs stand out as genuine works of art.",
+    name: "Ross Wilson",
+    title: "Director of Rock Groups",
+  },
+  {
+    quote:
+      "Liton Sheikh’s lyrics have a magical quality—they touch the soul and evoke emotions effortlessly. His ability to craft beautiful melodies and thought-provoking lyrics is truly commendable. Liton’s songs resonate with authenticity, they feel like personal stories shared with the world. Liton’s words have the power to heal hearts and ignite passion. Thank you, Liton, for sharing your gift with us. Your music brightens our days. In a world filled with noise, Liton Sheikh’s songs stand out as genuine works of art.",
+    name: "Ross Wilson",
+    title: "Director of Rock Groups",
+  },
+  {
+    quote:
+      "Liton Sheikh’s lyrics have a magical quality—they touch the soul and evoke emotions effortlessly. His ability to craft beautiful melodies and thought-provoking lyrics is truly commendable. Liton’s songs resonate with authenticity, they feel like personal stories shared with the world. Liton’s words have the power to heal hearts and ignite passion. Thank you, Liton, for sharing your gift with us. Your music brightens our days. In a world filled with noise, Liton Sheikh’s songs stand out as genuine works of art.",
+    name: "Ross Wilson",
+    title: "Director of Rock Groups",
+  },
+];
+
+export const companies = [
+  // {
+  //   id: 1,
+  //   name: "Youtube",
+  //   img: "/cloud.svg",
+  //   nameImg: "/cloudName.svg",
+  // },
+  // {
+  //   id: 2,
+  //   name: "Netflix",
+  //   img: "/app.svg",
+  //   nameImg: "/appName.svg",
+  // },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Bank Manager",
+    desc: "Worked as a supervisor in IMB Bank of Australia around a decade.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Entrepreneur",
+    desc: "Opened several businesses based in Australia and Bangladesh.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Actor",
+    desc: "I have participaited in many films staring renowned movie stars",
+    className: "md:col-span-2 ", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Influencer",
+    desc: "I post blogs and songs actively in Youtube, Facebook and Instagram.                               ",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/yt.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg" ,
+    
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+  {
+    id: 4,
+    img: "/fb.svg"
+  },
+  {
+    id: 5,
+    img: "/whatsapp.svg"
+  },
+];
